d2515f17997cfc4e9aa5017f26622e14e233106c
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 13:28:08 -0500

    fix: closing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,8 +9,8 @@ shared_scripts {
 	'@ox_lib/init.lua',
 	'@qbx_core/modules/utils.lua',
 	'@qbx_core/shared/locale.lua',
-	'locales/en.lua'
-	'locales/*.lua'
+	'locales/en.lua',
+	'locales/*.lua',
 	'config.lua',
 }
 
